678a62b4439f3edbf389b96403a6bad3233b35a8
Daniel Flores <d.flores042@gmail.com>
2019-01-27 21:21:30 -0500

    implement google maps api

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 function initMap(){
-  var options = {
-    zoom:8,
-    center: {lat:40.7128,lng:74.0060}
-  }
 
+  //map options
+  var options = {
+    zoom:10,
+    center: {lat:40.7128,lng:-74.0060}
+  }
+  
+  //new map
   var map = new
   google.maps.Map(document.getElementById('map'), options);
+
+  //add marker
+  var marker = new google.maps.Marker({
+    position:{lat:40.6782,lng:-73.9422},
+    map:map
+  });
+
+  var infoWindow = new google.maps.InfoWindow({
+    content:`<h1>Brooklyn</h1>`
+  });
+
+  marker.addListener('click', function(){
+    infoWindow.open(map, marker);
+  });
 }
